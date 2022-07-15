70f5d5b0e1e6ea1a418281a6aabdaa11f7fbbf9f
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-07-15 16:11:13 +0100

    feat(heat.jl): a load of tidying and sprucing

--- heat.jl
+++ heat.jl
@@ -1,3 +1,4 @@
+#!/usr/bin/env julia
 #
 # PROGRAM: heat equation solve
 #
@@ -32,43 +33,31 @@ import LinearAlgebra: norm
 
 const LENGTH = 1000
 
-function main(n = 1000, nsteps = 10)
-    # Set problem definition
-    α = 0.1          # heat equation coefficient
-    dx = LENGTH / (n + 1)  # physical size of each cell (+1 as don't simulate boundaries as they are given)
-    dt = 0.5 / nsteps    # time interval (total time of 0.5s)
-
+function main(n = 1000, nsteps = 10, α = 0.1, δx = LENGTH / (n + 1), δt = 0.5 / nsteps)
     # Print message detailing runtime configuration
-    totaltime = dt * nsteps
-    @info "MMS heat equation starting..." (n, n) dx (LENGTH, LENGTH) α nsteps totaltime dt
+    totaltime = δt * nsteps
+    @info "MMS heat equation starting..." (n, n) δx (LENGTH, LENGTH) α nsteps totaltime δt
 
-    # Stability requires that dt/(dx^2) <= 0.5
-    r = α * dt / dx^2
+    # Stability requires that δt/(dx^2) <= 0.5
+    r = α * δt / δx^2
     if (r > 0.5)
         @warn "Stability: unstable" r
     end
 
-    # Allocate two nxn grids
-    u = zeros(n, n)
-    u_tmp = zeros(n, n)
-    tmp = 0.0
-
-    # Set the initial value of the grid under the MMS scheme
-    initial_value!(u, dx)
+    u = initial_value((n, n), δx)
+    u_tmp = Array{Float64}(undef, (n, n))
 
     # Run through timesteps under the explicit scheme
+    B = zeros(n, n) # temporary allocation
     @time for t = 2:nsteps
-        # Call the solve kernel
-        # Computes u_tmp at the next timestep
-        # given the value of u at the current timestep
-        solve!(u_tmp, α, dx, dt, u)
+        solve!(u_tmp, B, u, α, δx, δt)
         u, u_tmp = u_tmp, u
     end
 
     # Check the L2-norm of the computed solution
     # against the *known* solution from the MMS scheme
     @time begin
-        themask = mask(size(u), dt * nsteps, α, dx)
+        themask = mask(size(u), δt * nsteps, α, δx)
         l2norm = norm(u .- themask)
     end
 
@@ -80,55 +69,44 @@ end
 """
 Sets the mesh to an initial value, determined by the MMS scheme
 """
-function initial_value!(u, dx)
-    for j = 2:size(u)[2]
-        for i = 2:size(u)[1]
-            u[i, j] = sin(pi * dx * (i - 1) / LENGTH) * sin(pi * dx * (j - 1) / LENGTH)
-        end
+function initial_value((x, y), δx)
+    u = zeros((x, y))
+    for (i, j) in Iterators.product(axes(u[1:end-1, 1:end-1])...)
+        u[i+1, j+1] = sin(pi * δx * i / LENGTH) * sin(pi * δx * j / LENGTH)
     end
+    return u
 end
 
 
 """
 Compute the next timestep, given the current timestep
 """
-function solve!(u_tmp, α, dx, dt, u)
+function solve!(uₜ::AbstractArray, B::AbstractArray, u::AbstractArray, α, δx, δt)
     # Finite difference constant multiplier
-    r = α * dt / dx^2
-    r2 = 1 - 4r
+    r = α * δt / δx^2
+    r₂ = 1 - 4r
 
-    xmax = size(u_tmp)[1]
-    ymax = size(u_tmp)[2]
+    (xₘ, yₘ) = size(u)
+    B[2:end-1, 2:end-1] .= (
+        u[i+1, j] + u[i, j-1] + u[i-1, j] + u[i, j+1] for
+        (i, j) in Iterators.product(2:xₘ-1, 2:yₘ-1)
+    )
 
-    # Loop over the nxn grid
-    for j = 2:ymax
-        for i = 2:xmax
-            # Update the 5-point stencil, using boundary conditions on the edges of the domain.
-            # Boundaries are zero because the MMS solution is zero there.
-            u_tmp[i, j] =
-                r2 * u[i, j] +
-                r * (
-                    (i < xmax ? u[i+1, j] : 0.0) +
-                    (i > 1 ? u[i-1, j] : 0.0) +
-                    (j < ymax ? u[i, j+1] : 0.0) +
-                    (j > 1 ? u[i, j-1] : 0.0)
-                )
-        end
-    end
+    @. uₜ[:, :] = r₂ * u + r * B
 end
 
 
 """
 Matrix of true answers given by the manufactured solution
 """
-function mask((xₘ, yₘ), t, α, dx)
-    modi = pi * dx / LENGTH
+function mask((xₘ, yₘ), t, α, δx)
+    modi = pi * δx / LENGTH
     xs = (1:xₘ-1) .* modi
     ys = (1:yₘ-1) .* modi
     Mₚ = Iterators.product(xs, ys)
     M₀ = zeros(xₘ, yₘ)
 
-    multiplier = exp(-2α * pi^2 * t / (LENGTH^2))
+    multiplier = exp(-2α * pi^2 * t * LENGTH^-2)
     gen((x, y)) = multiplier * sin(x) * sin(y)
 
     M₀[2:xₘ, 2:yₘ] = map(gen, Mₚ)
